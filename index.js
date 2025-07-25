13d4f2124fab6544bf257ea3e0a38fd48eb1e80b
ProjectWithRavi <raviyt73@gmail.com>
2025-07-25 22:56:22 +0530

    update port

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-import express from 'express';
-import sequelize from './config/database.js';
-import userRoutes from './routes/userRoutes.js';
+import express from "express";
+import sequelize from "./config/database.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 app.use(express.json());
 
 // Routes
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
 // Sync database and start server
 const PORT = process.env.PORT || 3000;
 
-sequelize.sync().then(() => {
-  console.log('Database synced');
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database synced");
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
   });
-}).catch((error) => {
-  console.error('Unable to connect to the database:', error);
-});
